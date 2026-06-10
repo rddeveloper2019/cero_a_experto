c776370290265771bd6d32bc55e7dc3643e96f78
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-06-11 01:42:16 +0300

    add other screens

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,11 @@
-export './home/home_screen.dart';
-export './buttons/buttons_screen.dart';
-export './cards/cards_screen.dart';
+export 'package:cero_a_experto/presentation/screens/animated/animated_screen.dart';
+export 'package:cero_a_experto/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:cero_a_experto/presentation/screens/buttons/buttons_screen.dart';
+export 'package:cero_a_experto/presentation/screens/cards/cards_screen.dart';
+export 'package:cero_a_experto/presentation/screens/home/home_screen.dart';
+export 'package:cero_a_experto/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:cero_a_experto/presentation/screens/progress/progress_screen.dart';
+export 'package:cero_a_experto/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:cero_a_experto/presentation/screens/ui_controls/ui_controls_screen.dart';
+
 
